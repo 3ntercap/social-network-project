e090d7b0c1a591d303572fd7cd83eda6dd7aa7b1
unrerum <3ntercap@gmail.com>
2023-09-21 23:20:24 +0300

    Add stylelint and i18n plugin for eslint

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,14 +1,19 @@
-import React from 'react';
-
-declare module '*.module.css';
-declare module '*.module.scss';
+declare module '*.scss' {
+    interface IClassNames {
+        [className: string]: string
+    }
+    const classNames: IClassNames;
+    export = classNames;
+}
 
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg' {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-    export default content;
+    import React from 'react';
+
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
 }
 
 declare const __IS_DEV__: boolean;

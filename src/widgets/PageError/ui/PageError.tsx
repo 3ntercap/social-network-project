882c98b221ceeb6e8fc47eec19ede7853192197b
unrerum <3ntercap@gmail.com>
2023-09-25 03:11:57 +0300

    add loki to ci pipeline

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -17,18 +17,7 @@ export const PageError = ({ className }: PageErrorProps) => {
 
     return (
         <div className={classNames('pageError', {}, [className])}>
-            <div className="noise" />
-            <div className="overlay" />
-            <div className="terminal">
-                <h1>
-                    Error
-                </h1>
-                <p className="output">
-                    Please try to
-                    <Button theme={ThemeButton.CLEAR} onClick={reloadPage}>[RELOAD]</Button>
-                </p>
-                <p className="output">Good luck.</p>
-            </div>
+            <Button theme={ThemeButton.CLEAR} onClick={reloadPage}>[RELOAD]</Button>
         </div>
     );
 };
